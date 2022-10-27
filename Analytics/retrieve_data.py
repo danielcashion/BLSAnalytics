f1ec0013f0aba69ce7c298264a275b2ca7e7f37c
danielcashion <daniel.cashion.nyc@gmail.com>
2022-10-27 14:29:25 -0400

    Initial Config Commit

---
+++ Analytics/retrieve_data.py
@@ -0,0 +1,11 @@
+# 
+# Name:     run_get_bls_data.py
+# Date:     April 14, 2022
+# Author:   Daniel Cashion
+#
+# Description:
+#
+# Use the bls_data class to obtain series of data
+# from the US Bureau of Labor Statistics (BLS) API.
+
+from bls_data_class.py import bls_data_class
